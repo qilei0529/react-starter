13364fda51527371a18c4663fa1d165d5b17afe7
qilei <qilei0529@qq.com>
2017-03-11 11:50:38 +0800

    增加 包引用 监控

--- README.md
+++ README.md
@@ -27,5 +27,10 @@
 
 ## 更新日志
 
+3月11日
+    增加 包引用 监控 wabpack-visualizer-plugin
+    可以 查看 自己的项目文件都引用了哪些包，包内容的占比，方便优化代码
+    运行的时候 查看 ./page/state.html
+
 3月3日 
     增加 动态 alias
--- build/webpack.dev.js
+++ build/webpack.dev.js
@@ -5,20 +5,24 @@
 const path = require('path');
 const webpack = require('webpack');
 const merge = require('webpack-merge');
+const visualizer = require('webpack-visualizer-plugin');
 
 const baseWebpackConfig = require('./webpack.base');
 
 // add hot-reload related code to entry chunks
 Object.keys(baseWebpackConfig.entry).forEach(function(name) {
-	baseWebpackConfig.entry[name] = ['webpack-hot-middleware/client?noInfo=true&reload=true', baseWebpackConfig.entry[name]]
-		// baseWebpackConfig.entry[name] = ['./build/dev-client'].concat(baseWebpackConfig.entry[name])
+    baseWebpackConfig.entry[name] = ['webpack-hot-middleware/client?noInfo=true&reload=true', baseWebpackConfig.entry[name]]
+        // baseWebpackConfig.entry[name] = ['./build/dev-client'].concat(baseWebpackConfig.entry[name])
 })
 
 module.exports = merge(baseWebpackConfig, {
-	devtool: '#eval-source-map',
-	plugins: [
-		new webpack.optimize.OccurrenceOrderPlugin(),
-		new webpack.HotModuleReplacementPlugin(),
-		new webpack.NoErrorsPlugin()
-	]
+    devtool: '#eval-source-map',
+    plugins: [
+        new webpack.optimize.OccurrenceOrderPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin(),
+        new visualizer({
+            filename: '../page/state.html'
+        })
+    ]
 })
